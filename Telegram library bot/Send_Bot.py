d1fd4dd9273c313d51177cb7a11694ab6e15c000
Mr Emoji <101939511+Emoji-DarkEYE@users.noreply.github.com>
2022-04-26 19:47:41 +0430

    Update Send_Bot.py

--- Telegram library bot/Send_Bot.py
+++ Telegram library bot/Send_Bot.py
@@ -1,6 +1,6 @@
 import requests
 
-# UrlBox: https://api.telegram.org/bot2044060648:AAEw59K7XRkf4oAhoSvP5WPTvLG7FErnR7Q/sendmessage?chat_id=1341257291&text=plase sub my channel on youthub
+# UrlBox: https://api.telegram.org/bot???????????????????????????????????????/sendmessage?chat_id=?????????&text=plase sub my channel on youthub
 # ContentTypeBox: 
 # ContentDataBox: 
 # HeadersBox: 
@@ -15,7 +15,7 @@ def make_MES(messege):
     try:
         #set your tocken & chat ID
         pm = str(messege)
-        sending_url = ("https://api.telegram.org/bot2044060648:AAEw59K7XRkf4oAhoSvP5WPTvLG7FErnR7Q/sendmessage?chat_id=1341257291&text="+pm)
+        sending_url = ("https://api.telegram.org/bot                    /sendmessage?chat_id=       &text="+pm)
 
 
         #data///
@@ -29,7 +29,7 @@ def make_MES(messege):
 
         requests.post("https://www.httpdebugger.com/tools/ViewHttpHeaders.aspx", data = Datadic)
     except:
-        print("Requests Not found chek problem")
+        print("You must apply the changes to the tool")
 
 
 
